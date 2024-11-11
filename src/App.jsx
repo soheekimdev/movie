838fe547a3a09641136c51ddbb6ddde66596613d
soheekimdev <soheekim.dev@gmail.com>
2024-11-11 10:53:53 +0900

    ♻️ Layout 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import Layout from './layouts/Layout';
 import Main from './pages/Main';
 import MovieDetail from './pages/MovieDetail';
-import NavBar from './components/NavBar';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
 function App() {
   return (
-    <div className="min-h-svh flex flex-col">
-      <NavBar />
-      <main className="flex-1 flex flex-col">
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/details/:id" element={<MovieDetail />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </main>
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Main />} />
+        <Route path="/details/:id" element={<MovieDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+
+export default function Layout() {
+  return (
+    <div className="min-h-svh flex flex-col">
+      <NavBar />
+      <main className="flex-1 flex flex-col">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
